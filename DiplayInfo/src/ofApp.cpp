f4f0a01430605e20c5ca60f5b046d2f42617eb3c
Makcime <marlier.maxime@gmail.com>
2015-09-24 10:55:57 +0200

    [enh]

--- DiplayInfo/src/ofApp.cpp
+++ DiplayInfo/src/ofApp.cpp
@@ -19,11 +19,11 @@ void ofApp::draw(){
 	base_tdf.draw(0, 0);
 	int pos = base_tdf.height + 20;
 	int a = 0;
-    ofDrawBitmapStringHighlight("X : " + ofToString(x_pos), 10,pos + (a++ * 20));  
-    ofDrawBitmapStringHighlight("Y : " + ofToString(y_pos), 10,pos + (a++ * 20));  
-    ofDrawBitmapStringHighlight("red : " + ofToString(pix_red), 10,pos + (a++ * 20));  
-    ofDrawBitmapStringHighlight("green  : " + ofToString(pix_green ), 10,pos + (a++ * 20));  
-    ofDrawBitmapStringHighlight("blue : " + ofToString(pix_blue), 10,pos + (a++ * 20));  
+    ofDrawBitmapStringHighlight("X     : " + ofToString(x_pos), 10,pos + (a++ * 20));  
+    ofDrawBitmapStringHighlight("Y     : " + ofToString(y_pos), 10,pos + (a++ * 20));  
+    ofDrawBitmapStringHighlight("red   : " + ofToString(pix_red), 10,pos + (a++ * 20));  
+    ofDrawBitmapStringHighlight("green : " + ofToString(pix_green ), 10,pos + (a++ * 20));  
+    ofDrawBitmapStringHighlight("blue  : " + ofToString(pix_blue), 10,pos + (a++ * 20));  
 }
 
 //--------------------------------------------------------------
